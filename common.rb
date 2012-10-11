e229b0a7864252aa36672accba4e8b11ef87ccf3
Corey Martella <cmartella@zynga.com>
2012-10-11 12:53:05 -0400

    more stuff

--- common.rb
+++ common.rb
@@ -22,3 +22,21 @@ def try_p(num,*args)
   load("p%03d.rb" % num) 
   send("p#{num}",*args)
 end
+def setup_p(num)
+  File.open("p%03d.rb" % num,'w') do |f|
+    f.puts "load 'common.rb'"
+    f.puts "def p#{num}(n=1)\n  \nend"
+  end unless File.exists?("p%03d.rb" % num)
+  append_desc(num)
+end
+def append_p_desc(num)
+  require 'rubygems'
+  require 'nokogiri'
+  doc = Nokogiri::HTML(open("http://projecteuler.net/problem=#{num}"))
+  prob = doc.css(".problem_content").text.strip.split(/\r?\n/)
+  File.open("p%03d.rb" % num,'a') do |f|
+    f.puts "\n"
+    f.puts prob.map{|l| "# #{l}"}.join("\n")
+  end
+
+end
--- p004.rb
+++ p004.rb
@@ -14,3 +14,5 @@ def p4(n)
   end
   r
 end
+# A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
