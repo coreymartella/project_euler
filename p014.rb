ebe6d5c14d43ee1f93a4d780ed2965a1a1f74407
Corey Martella <corey.martella@gmail.com>
2013-06-04 00:53:55 -0300

    Update p014.rb

--- p014.rb
+++ p014.rb
@@ -30,7 +30,8 @@ end
 #Brute force took 22s, optimized with lenghts of previous chains took 3s
 
 # The following iterative sequence is defined for the set of positive integers:
-# n n/2 (n is even)n  3n + 1 (n is odd)
+# n = n/2 (if n is even)
+# n  = 3n + 1 (if n is odd)
 # Using the rule above and starting with 13, we generate the following sequence:
 # 13  40  20  10  5  16  8  4  2  1
 # It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
